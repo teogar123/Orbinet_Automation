dbb38cf24523e28886e203546a1532d8b37724c5
teogar123 <teogar123@gmail.com>
2020-01-28 22:34:21 -0600

    Company Register -> Successfull

--- page_objects/LoginPage.js
+++ page_objects/LoginPage.js
@@ -1,6 +1,10 @@
 const locators = require('../locators/Locators')
 const actions = require('../base/Actions');
 const data = require('../test_data/Data');
+const protractorHelper = require('protractor-helper');
+const path = require('path');
+
+
 
 function LoginPage () {
     this.createAccountButton = element(by.xpath(CREATE_ACCOUNT_BUTTON));
@@ -14,6 +18,9 @@ function LoginPage () {
     this.emailToLginField = element(by.xpath("/html[1]/body[1]/div[6]/div[1]/div[2]/div[1]/input[1]"));
     this.passwordToLoginField = element(by.xpath("/html[1]/body[1]/div[6]/div[1]/div[2]/div[2]/input[1]"));
     this.loginToOrbiButton = element(by.xpath('/html[1]/body[1]/div[6]/div[1]/div[2]/div[4]/div[2]/b[1]/span[1]'));
+    this.orbiNetworwLogo = element(by.xpath("//img[@class='logo']"));
+    this.toUploadPhoto = element(by.xpath("//button[contains(@class,'btn button outline btnPicture')]"));
+    
     
 /**
  * 
@@ -52,6 +59,29 @@ function LoginPage () {
         browser.sleep(4000);
         actions.clickToElement(this.loginToOrbiButton);
         browser.sleep(4000);
+        if (this.orbiNetworwLogo.isDisplayed) {
+            console.log('The ORBI logo is Present-> ' + "Loggin : Successfull");
+        } else {
+            console.log("ORBI log is MISSING : Loggin Nor Succesfull");
+        };
+
+        browser.sleep(4000);
+        //helper.uploadFileIntoInputField(this.toUploadPhoto, this.absolutePath, 4000);
+        
+        const relativePathOfFileToUpload = "../page_obnjects/photo.png";
+        const absolutePathOfFileToUpload = path.resolve(__dirname, relativePathOfFileToUpload);
+
+        browser.get("https://qa.v2.orbinetwork.com/register/account");
+
+        const fileInputField = element
+        (by.xpath("/html[1]/body[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[3]/div[1]/button[1]"));
+        if (fileInputField.isDisplayed) {
+            console.log('The Element is present and Will Allow to Upload the require File -> : Success');
+        } else {
+            console.log('The Element is Not Diplayed and Will Allow to Upload the require file -> : Failed');
+        }
+        protractorHelper.uploadFileIntoInputField(fileInputField, absolutePathOfFileToUpload);
+        browser.sleep(6000);     
     }
 };
 module.exports = new LoginPage();
